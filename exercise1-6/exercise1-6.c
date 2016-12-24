53187c6beeb7472e822f20ec36cf24d86ca3c2f3
MovlaAliyev <eliyevmovlan@gmail.com>
2016-12-24 23:14:15 +0400

    exercise1-6

---
+++ exercise1-6/exercise1-6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/*Exercise 1.6 Verify that the expression getchar() != EOF 0 or 1*/
+
+
+int main(){ 
+
+ int c;
+   /* while ((c = getchar()) != EOF) {
+        printf("%d ", c != EOF);
+        putchar(c);
+    }*/
+    printf("\n%d\n", c != EOF);
+	printf("The value of EOF is %d Assuming senthil is a total cupper",EOF);
+
+	return 0;
+}
